117a4adaed157c97f7dbd478d5ef3906b2513032
Leo Vergnetti <tuj52136@cst11513.tu.temple.edu>
2019-02-14 11:06:06 -0500

    Initial Commit

---
+++ PicoFermiBagel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PicoFermiBagel
+//
+//  Created by Leo Vergnetti on 2/14/19.
+//  Copyright © 2019 Leo Vergnetti. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
